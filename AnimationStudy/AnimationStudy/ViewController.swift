22818daf355487c7523bdbbaf06eea3c75120973
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-12-07 23:30:27 +0900

    docs - 프로젝트 기본 세팅

---
+++ AnimationStudy/AnimationStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationStudy
+//
+//  Created by 이명진 on 12/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
