9c2556d13af7f94fe1171e5ea579581c6fef673a
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-12-07 23:55:08 +0900

    feat(FirstVC) - CGAffineTransform 연습

--- AnimationStudy/AnimationStudy/1stWeek/FirstVC.swift
+++ AnimationStudy/AnimationStudy/1stWeek/FirstVC.swift
@@ -10,29 +10,63 @@ import UIKit
 class FirstVC: UIViewController {
     
     private var testView : UIView = {
-        let view = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        let view = UIView(frame: CGRect(x: 150, y: 200, width: 50, height: 50))
         view.backgroundColor = .systemPink
         return view
     }()
     
     private let testButton : UIButton = {
-        let button = UIButton(frame: CGRect(x: 150, y: 400, width: 200, height: 50))
+        let button = UIButton(frame: CGRect(x: 150, y: 600, width: 200, height: 50))
         button.setTitle("짠!", for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 17)
         button.titleLabel?.textColor = .brown
         button.layer.backgroundColor = UIColor.black.cgColor
-        //        button.addTarget(self, action: #selector(didMoveTap), for: .touchUpInside)
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        didMoveTap()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
+        addTarget()
     }
     
     
     private func setUI() {
         view.backgroundColor = .white
+        view.addSubviews(testView, testButton)
     }
     
+    private func addTarget() {
+        testButton.addTarget(self, action: #selector(didMoveTap), for: .touchUpInside)
+    }
+    
+    @objc private func didMoveTap() {
+        
+        //animate 기본
+        UIView.animate(withDuration: 2.0, animations: { [self] in
+            testView.frame.origin.x += 100
+            testView.alpha = 0.3
+        })
+        
+        //CGAffineTransform
+        UIView.animate(withDuration: 2.0) { [self] in
+            testView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        }
+        UIView.animate(withDuration: 2.0) { [self] in
+            testView.transform = CGAffineTransform(rotationAngle: .pi)
+        }
+//        UIView.animate(withDuration: 2.0) { [self] in
+//            testView.transform = CGAffineTransform(translationX: 200, y: 200)
+//        }
+        
+        
+        self.testView.frame = CGRect(x: 150, y: 200, width: 50, height: 50)
+        // 초기화
+    }
 }
