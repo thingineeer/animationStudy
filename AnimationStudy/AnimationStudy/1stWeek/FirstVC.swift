311198c719cf978a0847a3aa81fd605f9d4f9419
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-12-09 21:51:31 +0900

    feat(FirstVC) - frame과 bound 차이 이해

--- AnimationStudy/AnimationStudy/1stWeek/FirstVC.swift
+++ AnimationStudy/AnimationStudy/1stWeek/FirstVC.swift
@@ -17,7 +17,7 @@ class FirstVC: UIViewController {
     
     private let testButton : UIButton = {
         let button = UIButton(frame: CGRect(x: 150, y: 600, width: 200, height: 50))
-        button.setTitle("짠!", for: .normal)
+        button.setTitle("버튼", for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 17)
         button.titleLabel?.textColor = .brown
         button.layer.backgroundColor = UIColor.black.cgColor
@@ -50,17 +50,18 @@ class FirstVC: UIViewController {
         
         //animate 기본
         UIView.animate(withDuration: 2.0, animations: { [self] in
-            testView.frame.origin.x += 100
+            testView.frame.origin.x += 100 // frame은 상위뷰
+            print(testView.frame.origin)
             testView.alpha = 0.3
         })
         
-        //CGAffineTransform
-        UIView.animate(withDuration: 2.0) { [self] in
-            testView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
-        }
-        UIView.animate(withDuration: 2.0) { [self] in
-            testView.transform = CGAffineTransform(rotationAngle: .pi)
-        }
+        //CGAffineTransform /// 움직일때 추가적으로 변하는 것들?
+//        UIView.animate(withDuration: 2.0) { [self] in
+//            testView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+//        }
+//        UIView.animate(withDuration: 2.0) { [self] in
+//            testView.transform = CGAffineTransform(rotationAngle: .pi)
+//        }
 //        UIView.animate(withDuration: 2.0) { [self] in
 //            testView.transform = CGAffineTransform(translationX: 200, y: 200)
 //        }
