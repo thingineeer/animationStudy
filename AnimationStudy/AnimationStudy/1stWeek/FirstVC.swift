3fd8740bf7cbee47779c6b2b2a40325abf10f9e5
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-12-11 19:20:41 +0900

    feat(FirstVC) - concatenating 사용해보기

--- AnimationStudy/AnimationStudy/1stWeek/FirstVC.swift
+++ AnimationStudy/AnimationStudy/1stWeek/FirstVC.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class FirstVC: UIViewController {
     
-    private var testView : UIView = {
-        let view = UIView(frame: CGRect(x: 150, y: 200, width: 50, height: 50))
+    private let testView : UIView = {
+        let view = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         view.backgroundColor = .systemPink
         return view
     }()
@@ -27,7 +27,6 @@ class FirstVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
-        didMoveTap()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -49,25 +48,61 @@ class FirstVC: UIViewController {
     @objc private func didMoveTap() {
         
         //animate 기본
-        UIView.animate(withDuration: 2.0, animations: { [self] in
-            testView.frame.origin.x += 100 // frame은 상위뷰
-            print(testView.frame.origin)
-            testView.alpha = 0.3
-        })
+        //        UIView.animate(withDuration: 2.0, animations: { [self] in
+        //            testView.frame.origin.x += 100 // frame은 상위뷰
+        //            print(testView.frame.origin)
+        //            testView.alpha = 0.3
+        //        })
         
-        //CGAffineTransform /// 움직일때 추가적으로 변하는 것들?
-//        UIView.animate(withDuration: 2.0) { [self] in
-//            testView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
-//        }
-//        UIView.animate(withDuration: 2.0) { [self] in
-//            testView.transform = CGAffineTransform(rotationAngle: .pi)
-//        }
-//        UIView.animate(withDuration: 2.0) { [self] in
-//            testView.transform = CGAffineTransform(translationX: 200, y: 200)
-//        }
+        //CGAffineTransform
         
+        //        /// scale : 뷰의 넓이와 높이를 두 배(2.0)로 증가 시킵니다.
+        //        UIView.animate(withDuration: 2.0) { [self] in
+        //            testView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        //        }
+        //        /// rotationAngle 사용 뷰를 180도 회전 시킵니다.
+        //        UIView.animate(withDuration: 2.0) { [self] in
+        //            testView.transform = CGAffineTransform(rotationAngle: .pi)
+        //        }
+        //        /// translationX 뷰의 위치를 X 200 Y 200 움직입니다.
+        //        UIView.animate(withDuration: 2.0) { [self] in
+        //            testView.transform = CGAffineTransform(translationX: 200, y: 200)
+        //        }
         
-        self.testView.frame = CGRect(x: 150, y: 200, width: 50, height: 50)
+        concatenationing()
+        
         // 초기화
+    }
+    
+    private func concatenationing() {
+//        
+//        UIView.animate(withDuration: 2.0) { [self] in
+//            testView.backgroundColor = .gray
+//            
+//            let scale = CGAffineTransform(scaleX: 2.0, y: 2.0)
+//            let rotate = CGAffineTransform(rotationAngle: .pi)
+//            let move = CGAffineTransform(translationX: 200, y: 200)
+//            
+//            let combine = scale.concatenating(rotate).concatenating(move)
+//            
+//            testView.transform = combine
+//        }
+        
+        UIView.animate(withDuration: 2.0, animations: { [self] in
+            testView.backgroundColor = .gray
+            
+            let scale = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            let rotate = CGAffineTransform(rotationAngle: .pi)
+            let move = CGAffineTransform(translationX: 200, y: 200)
+            
+            let combine = scale.concatenating(rotate).concatenating(move)
+            
+            testView.transform = combine
+        }) { (_) in
+            UIView.animate(withDuration: 2.0) {
+                self.testView.transform = CGAffineTransform.identity
+            }
+        }
+
     }
 }
